7782b53f84b1ce99745c9cc2ed7030fc011806da
picossi <viniciuspicossi@gmail.com>
2018-09-24 20:03:39 -0300

    ferramenta para organizar as amostras por clusters

---
+++ images.by.clusters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace images.by.clusters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo[] allFiles = Directory.EnumerateFiles(@"D:\MEGAsync\dataset front", "*.*", SearchOption.AllDirectories)
+                .AsParallel()
+                .Where(w => w.EndsWith(".png") || w.EndsWith(".jpg") || w.EndsWith(".gif"))
+                .Select(s => new FileInfo(s))
+                .ToArray();
+
+            for (int k = 4; k <= 7; k++)
+            {
+                for (int c = 1; c <= k; c++)
+                {
+                    string source = $@"D:\MEGAsync\dataset front features\k{k}\c{c}";
+                    string target = $@"D:\MEGAsync\dataset front by clusters\k{k}\c{c}";
+
+                    FileInfo[] files = Directory.EnumerateFiles(source, "*.*", SearchOption.AllDirectories)
+                        .AsParallel()
+                        .Where(w => w.EndsWith(".csv"))
+                        .Select(s => new FileInfo(s))
+                        .ToArray();
+
+                    files.AsParallel().ForAll(f =>
+                    {
+                        var file = allFiles.AsParallel().Where(w => f.Name.StartsWith(w.Name)).SingleOrDefault();
+                        if (file != null)
+                            file.CopyTo($@"{target}\{file.Name}", true);
+                    });
+                }
+            }
+        }
+    }
+}
